e37d3845c9b0412e01b0239e46b8c26d149d680a
AlbericoJr <40373171+AlbericoJr@users.noreply.github.com>
2025-11-11 17:30:04 -0300

    Nova - Criação do Hero, Services, About

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,9 +1,192 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import heroVideo from '../assets/Images/hero-video.mp4'
+import title_icon from '../assets/Images/title_icon.svg'
+import ser1 from '../assets/Images/serv-icon1.png'
+import ser2 from '../assets/Images/serv-icon2.png'
+import ser3 from '../assets/Images/serv-icon3.png'
+import ser4 from '../assets/Images/serv-icon4.png'
+
+import {Link} from 'react-router-dom'
+import {Icon} from '@iconify/react'
 
 export default function Index() {
+  const [activeIndex, setActiveIndex] = useState(1)
+
+  const services = [
+    {id: 1, title: "Criação", img: ser1},
+    {id: 2, title: "Websites", img: ser2},
+    {id: 3, title: "Estúdio", img: ser3},
+  ]
+
   return (
-    <div>
-      index
-    </div>
+    <>
+      {/* Hero */}
+      <div className='h-screen w-full relative overflow-hidden'>
+        <video 
+          autoPlay
+          loop
+          muted
+          playsInline
+          className='absolute top-0 left-0 w-full h-full object-cover'
+        >
+          <source src={heroVideo} type='video/mp4' />
+        </video>
+
+        <div className='px-[2%] md:px-[8%] xl:px-[12%] text-white h-full w-full flex flex-col justify-center relative z-10'>
+          <h2 className='text-5xl md:text-7xl xl:text-9xl font-semibold w-full lg:max-w-2xl pb-3 relative'>
+            Design do Futuro
+            <span className='absolute top-0 right-0 w-10 h-10'>
+              <img src={title_icon} alt="Icon"  className='w-full h-full'/>
+            </span>
+          </h2>
+
+          <p className='text-xl md:text-2xl max-w-2xl font-normal'>
+            Estúdio criativo na intersecção entre arte, design e tecnologia.
+          </p>
+
+          <a 
+            href="#"
+            className='absolute right-20 bottom-16 w-[67px] h-[67px] rounded-full border border-white z-30 cursor-pointer flex items-center justify-center group hover:scale-110 transition-transform duration-300'
+          >
+            <span className='absolute inset-0 rounded-full border border-white opacity-50 animate-pulseRing'></span>
+
+            <span className='relative flex items-end justify-between w-[22px] h-[22px] z-10'>
+              <span className='w-1 bg-white animate-eq1'></span>
+              <span className='w-1 bg-white animate-eq2'></span>
+              <span className='w-1 bg-white animate-eq3'></span>
+              <span className='w-1 bg-white animate-eq4'></span>
+            </span>
+          </a>
+        </div>
+      </div>
+
+      {/* Services */}
+      <div className='services grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 py-[8%] px-[2%] md:px-[8%] xl:px-[12%]'>
+        {services.map((services, index) =>(
+          <div
+            key={services.id}
+            className={`w-full text-center group rounded-full p-10 flex justify-center items-center flex-col relative service-item h-[450px] transition-all duration-300
+              ${activeIndex === index ? "active bg-primary" : "bg-transparent"}`}
+              onMouseEnter={() => setActiveIndex(index)}
+              onMouseLeave={() => setActiveIndex(1)}
+          >
+            <img 
+              src={services.img} 
+              alt={services.title}
+              width={60}
+              height={60}
+              className={`pb-4 transition-all duration-300 ${activeIndex === index ? "filter-none" : "invert"}`}
+            />
+
+            <div 
+              className={`service-content transition-all duration-300 
+              ${activeIndex === index ? "text-black" : "text-white"}`}
+            >
+              <h2 className='text-5xl font-semibold pb-6'>{services.title}</h2>
+              <p className='text-xl'>
+              Desenvolver websites é muito mais do que marketing; é também estética.
+              </p>
+            </div>
+
+            {/* Bubbles */}
+            {/* <div className='nova-bubble absolute bottom-[-60px] right-0 w-[142px] h-[152px] pointer-events-none'>
+              <div 
+                className={`bubble-1 w-8 h-8 rounded-full bg-pink-300 absolute transition-all duration-500 
+                ${activeIndex == index 
+                  ? "opacity-100 bottom-0 right-0 scale-100" 
+                  : "opacity-0 bottom-[-20%] right-[-10%] scale-0"}`
+              }></div>
+              
+              <div 
+                className={`bubble-2 w-6 h-6 rounded-full bg-blue-300 absolute transition-all duration-500 delay-100
+                ${activeIndex == index 
+                  ? "opacity-100 bottom-[30%] left-0 scale-100" 
+                  : "opacity-0 bottom-[-50%] left-[-40%] scale-0"}`
+              }></div>
+              
+              <div 
+                className={`bubble-3 w-10 h-10 rounded-full bg-green-300 absolute transition-all duration-500 delay-200
+                ${activeIndex == index 
+                  ? "opacity-100 bottom-[63%] left-[57%] scale-100" 
+                  : "opacity-0 bottom-[-50%] left-[-40%] scale-0"}`
+              }></div>
+            </div> */}
+          </div>
+        ))}
+      </div>
+
+      {/* About */}
+      <div className='about py-[8%] px-[2%] md:px-[8%] xl:px-[12%] flex justify-between items-star lg:flex-row flex-col gap-10'>
+        <div className='about-content w-full lg:w-[60%]'>
+          <span className='text-black bg-primary px-2 py-3 font-semibold text-md sm:text-xl rounded-sm'>Bem-vindos à Nova</span>
+          <h2 className='text-3xl sm:text-5xl font-semibold sm:max-w-3xl my-6 leading-tight text-white'>
+            Uma Agência de Design que Gera Sucesso Conquistando Corações.
+          </h2>
+
+          <ul className='flex xl:flex-nowrap flex-wrap gap-5 lg:gap-10'>
+            <li className='w-full xl:w-1/2'>
+              <span className='font-semibold text-2xl text-white'>Nossa Missão</span>
+              <p className='text-md sm:text-md mt-2 text-gray-300'>
+                Desde o momento em que nossa empresa foi fundada, temos ajudado nossos clientes a encontrar <b>soluções excepcionais para seus negócios</b> marcas memoráveis e produtos digitais. Nossa expertise cresce a cada ano, assim como nossa experiência acumulada.
+              </p>
+            </li>
+            
+            <li className='w-full xl:w-1/2'>
+              <span className='font-semibold text-2xl text-white'>Nosso Objetivo</span>
+              <p className='text-md sm:text-md my-2 text-gray-300'>
+                Nosso objetivo é entregar experiências incríveis que façam as pessoas falarem, e construir valor estratégico para marcas, tecnologia e entretenimento.
+              </p>
+              <Link to="/about" className='btn rounded-sm'>
+                <Icon icon="vaadin:plus" width="30" height="30"/>
+                <span>Mais Sobre Nós</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className='w-full lg:w-[40%] flex justify-center items-center'>
+          <div className='flex items-center justify-center w-[250px] h-[250px] relative border rounded-full'>
+            <svg 
+              viewBox="0 0 300 300"
+              className='absolute w-full h-full animate-[spin_20s_linear_infinite]'
+            >
+              <defs>
+                <path 
+                  id="circlePath"
+                  d="M150,150 m-120,0 a120,120 0 1,1 240,0 a120,120 0 1,1 -240,0"
+                ></path>
+              </defs>
+
+              <text 
+                fill='#fff'
+                fontSize="16"
+                fontWeight="600"
+                letterSpacing="4"
+                textLength="1400"
+              >
+                <textPath href='#circlePath' startOffset="0">
+                ANOS DE EXPERIÊNCIA EM SOLUÇÕES DIGITAIS . ANOS DE EXPERIÊNCIA EM SOLUÇÕES DIGITAIS . 
+                </textPath>
+              </text>
+
+            </svg>
+            <div className='border rounded-[50%] px-6 py-5'>
+              <div 
+                className='text-8xl font-bold text-transparent mt-5 border'
+                style={{
+                  WebkitTextStrokeWidth: "3px",
+                  WebkitTextStrokeColor: "#fff",
+                }}
+              >
+                14
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Splide */}
+    </>
   )
 }
